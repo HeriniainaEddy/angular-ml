b25dc707aa8846dfeb211b251d2df66d198779f5
Heriniaina <heriniaina.eddy10@gmail.com>
2020-07-20 17:57:12 +0300

    Update package.json

--- server.js
+++ server.js
@@ -5,15 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the angularapp directory
-// app.use(express.static(__dirname + '/angularml'));
-app.use(express.static('./dist'));
+app.use(express.static(__dirname + '/dist'));
 
-// app.get('/*', function(req,res) {
-//     res.sendFile(path.join(__dirname+'/angularml/index.html'));
-// });
-
-app.get('/*', function (req, res) {
-    res.sendFile('index.html', { root: 'dist' });
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
